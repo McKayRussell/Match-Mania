44e3a63945b267b822fcc733de5be0438063f79c
SMerino13 <83145646+SMerino13@users.noreply.github.com>
2021-09-30 11:30:28 -0700

    Made my font bigger

--- smerino.cpp
+++ smerino.cpp
@@ -3,7 +3,7 @@
 void show_steven_credits(int x, int y)
 {
     Rect r;
-    r.bot = y + 20;
+    r.bot = y - 40;
     r.left = x;
     r.center = 1;
     ggprint16(&r, 16, 0x00ff0000, "Steven was def not here");
