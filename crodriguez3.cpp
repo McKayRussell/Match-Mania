b9b773b9aab7f3440166e2a2d1745c354658e867
ClementeRodriguez <49183998+ClementeRodriguez@users.noreply.github.com>
2021-09-30 11:35:40 -0700

    Add files via upload

---
+++ crodriguez3.cpp
@@ -0,0 +1,10 @@
+#include "fonts.h"
+
+void show_clementes_credits(int x, int y)
+{
+    Rect r;
+    r.bot = y;
+    r.left = x;
+    r.center = 1;
+    ggprint8b(&r, 16, 0x00ff0000, "Clemente is caliente");
+}
