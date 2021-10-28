65526fb723d68e04a55503134e35eb969c81a7d1
dpham8 <89600390+dpham8@users.noreply.github.com>
2021-10-28 12:29:59 -0700

    Add files via upload

--- dpham.cpp
+++ dpham.cpp
@@ -1,5 +1,19 @@
 //Dat Pham
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <GL/glx.h>
+#include "log.h"
+//#include "ppm.h"
 #include "fonts.h"
+#include <iostream>
+#include <unistd.h>
+#include <bits/stdc++.h>
+
 
 void show_dat_credits(int x, int y)
 {
@@ -10,4 +24,37 @@ void show_dat_credits(int x, int y)
     ggprint16(&r, 16, 0x00000000, "Dat fat cat sat at a mat"); 
 
 }
+//GLuint texture;
+void drawBackground(int x, int y, GLuint texture) {
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glBegin(GL_QUADS);
+	    glTexCoord2f(0.0f, 1.0f); glVertex2i(0, 0);
+	    glTexCoord2f(0.0f, 0.0f); glVertex2i(0, y);
+	    glTexCoord2f(1.0f, 0.0f); glVertex2i(x, y);
+	    glTexCoord2f(1.0f, 1.0f); glVertex2i(x, 0);
+    glEnd(); 
+    glPopMatrix();
+}
+
+/*
+//make struct rectangles for texture maps
+void makeCards(int row, int col, int x, int y, Card) {
+    for (int i=0; i<row; i++) {
+        for (int j=0; j<col; j++) {
+            //g.cards[i][j].xbox = x + (j*100.0f);
+            //g.cards[i][j].ybox = y - (i*130.0f);
+        }
+    } 
+}
+
+void dat_note(int x, int y)
+{
+    Rect r;
+    r.bot = y;
+    r.left =x;
+    r.center = 1;
+    ggprint16(&r, 16, 0x00000000, "I put my name next to my functions in the comments."); 
+}
+*/
+
 
