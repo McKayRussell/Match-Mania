1ac83afc1943876361747300ea89ff21c3fd269b
ClementeRodriguez <crodriguez174@csub.edu>
2021-10-29 00:17:38 -0700

    added seasonal theme function

--- crodriguez3.cpp
+++ crodriguez3.cpp
@@ -9,3 +9,9 @@ void show_clementes_credits(int x, int y)
     r.center = 1;
     ggprint16(&r, 16, 0x00000000, "Clemente is caliente");
 }
+
+void set_seasonal_theme(image img)
+{
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, img[10].width, img[10].height,
+									0, GL_RGB, GL_UNSIGNED_BYTE, img[10].data);
+}
