ac0087a1a05852a5f56837080da496953166e0f3
SMerino13 <83145646+SMerino13@users.noreply.github.com>
2021-10-29 10:26:14 -0700

    softfix

--- crodriguez3.cpp
+++ crodriguez3.cpp
@@ -10,8 +10,8 @@ void show_clementes_credits(int x, int y)
     ggprint16(&r, 16, 0x00000000, "Clemente is caliente");
 }
 
-void set_seasonal_theme(image img)
-{
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, img[10].width, img[10].height,
-									0, GL_RGB, GL_UNSIGNED_BYTE, img[10].data);
-}
+// void set_seasonal_theme(image img)
+// {
+//     glTexImage2D(GL_TEXTURE_2D, 0, 3, img[10].width, img[10].height,
+// 									0, GL_RGB, GL_UNSIGNED_BYTE, img[10].data);
+// }
